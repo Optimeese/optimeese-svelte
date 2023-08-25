4ad50061dfca82978af72fcd1064f94cf680ac11
axel-rock <rockaxel@gmail.com>
2023-08-25 17:05:33 +0200

    Commented use of environment variables

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,5 @@
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
-import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit'
+// import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
+// import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit'
 import type { Handle } from '@sveltejs/kit'
 
 export const handle: Handle = async ({ event, resolve }) => {
