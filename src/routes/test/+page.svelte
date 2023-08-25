56a66fb148edaf359d2ef564f400f95828dbb37f
axel-rock <rockaxel@gmail.com>
2023-08-25 17:11:10 +0200

    Test to put all dependencies outside devDependencies

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { PageData } from './$types'
+
+	export let data: PageData
+</script>
+
+{#await data.awaited.results}
+	<p>Waiting for data...</p>
+{:then data}
+	<p>Received data:</p>
+	<pre>{JSON.stringify(data, null, 2)}</pre>
+{:catch error}
+	<p>Error: {error.message}</p>
+{/await}
