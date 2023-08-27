a6610700c260900e3969f0f9c584aeb8c9e93ad1
axel-rock <rockaxel@gmail.com>
2023-08-27 19:00:50 +0200

    Try reset the project

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.1a5f0d98.js","_app/immutable/chunks/scheduler.4b895fe1.js","_app/immutable/chunks/index.bbdfbc60.js","_app/immutable/chunks/singletons.e591ae40.js"];
-export const stylesheets = [];
-export const fonts = [];
--- src/routes/+layout.server.ts
+++
@@ -1,5 +0,0 @@
-import type { LayoutServerLoadEvent } from './$types'
-
-export const load = async ({ locals }: LayoutServerLoadEvent) => {
-	return { user: locals.user }
-}
